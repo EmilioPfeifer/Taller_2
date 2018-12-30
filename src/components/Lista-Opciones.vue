6247ecb36c4e2a3398ff44bb75f719b4fc34a5e9
EmilioPfeifer <e.pfeifer01@ufromail.cl>
2018-12-29 22:05:59 -0300

    Vuetify

---
+++ src/components/Lista-Opciones.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-container grid-list-md text-xs-center>
+    <v-layout row>
+        <v-flex xs12 sm6 offset-sm3>
+            <b-list-group>
+                <b-list-group-item href="">Clientes</b-list-group-item>
+                <b-list-group-item href="">Lista de Farmacos</b-list-group-item>
+                <b-list-group-item href="">Agregar Recetas</b-list-group-item>
+            </b-list-group >
+        </v-flex>
+    </v-layout>
+    </v-container>
+</template>
